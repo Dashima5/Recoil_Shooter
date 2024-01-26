80809e007114e0649065b9fc47359798847c9daa
Dashima5 <133100094+Dashima5@users.noreply.github.com>
2024-01-26 14:50:30 +0900

    컨셉변경: 우주유영
    -중력이 있는 상황에서의 플랫포머가 아닌, 중력과 공기저항이 없는 우주 공간에서의 슈팅게임으로 컨셉 변경: 작은 이동 힘이라도 계속 주면 강해지고, 조작이나 충돌없이는 속력이 줄어들지 않는 것을 특이한 조작감으로 받아들임.
    -wasd를 이용한 4방향 이동
    -spacebar로 전체 정지(velocity값을 반으로 줄여가며 점진적으로 정지하게 만드려고 하나, 현제는 체감하지 못할 정도로 일순간 정지해 수정 필요)
    -wasd,스페이스바, 마우스 이동의 최대 가능 속력
    -wasd를 땔때, 혹은 스페이스바를 누르는 동안 x또는 y방향 절대값 속력이 buttonMinspeed이하면 그 방향 velocity를 0으로 초기화

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,67 +6,66 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private Vector3 mouseWorldPos;
-    private Vector3 selfPos;
     private Vector3 selfScreenPos;
-    private Rigidbody2D selfRb;
-    public GameObject pointer;
+    private Rigidbody2D Rb;
+    private Transform weaponHolder;
     public GameObject bulletPrefap;
-    private float bulletspeed = 20f;
-    private bool Gslam = false;
+    public float bulletspeed = 20f;
+    public float buttonMaxspeed = 4.8f;
+    public float buttonMinspeed = 1f;
+    public float mouseMaxspeed = 15f;
     //private float GPdamage = 0f;
     void Start()
     {
-        selfRb = GetComponent<Rigidbody2D>();
+        Rb = GetComponent<Rigidbody2D>();
+        weaponHolder = transform.Find("weaponHolder");
     }
 
     void Update()
     {
         mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        selfPos = transform.position;
-        Vector3 direction = mouseWorldPos - selfPos;
+        Vector3 direction = mouseWorldPos - transform.position;
 
-        Vector3 rotate = mouseWorldPos - pointer.transform.position;
+        Vector3 rotate = mouseWorldPos - weaponHolder.position;
         float rotZ = Mathf.Atan2(rotate.y, rotate.x) * Mathf.Rad2Deg;
-        pointer.transform.rotation = Quaternion.Euler(0,0,rotZ);
+        weaponHolder.rotation = Quaternion.Euler(0,0,rotZ);
 
         if (Input.GetMouseButtonDown(0)) {
-            if (Gslam) { selfRb.velocity = Vector3.zero; Gslam = false; }
             selfScreenPos = Camera.main.WorldToScreenPoint(transform.position);
             Vector3 MoveDir = (Vector3)(Input.mousePosition-selfScreenPos);
             MoveDir.Normalize();
             MoveDir.z = 0;
-            selfRb.AddForce(-MoveDir*10,ForceMode2D.Impulse);
+            Rb.AddForce(-MoveDir*10,ForceMode2D.Impulse);
+            if(Rb.velocity.x > mouseMaxspeed) { Rb.velocity = new Vector3(mouseMaxspeed, Rb.velocity.y,0); }
+            else if(Rb.velocity.x < -mouseMaxspeed) { Rb.velocity = new Vector3(-mouseMaxspeed, Rb.velocity.y, 0); }
+            if (Rb.velocity.y > mouseMaxspeed) { Rb.velocity = new Vector3(Rb.velocity.x,mouseMaxspeed,0); }
+            else if (Rb.velocity.y < -mouseMaxspeed) { Rb.velocity = new Vector3(Rb.velocity.x, -mouseMaxspeed, 0); }
+
             GameObject newbullet = Instantiate(bulletPrefap) as GameObject;
             newbullet.transform.position = transform.position;
             newbullet.transform.rotation = Quaternion.Euler(0, 0, rotZ);
             newbullet.GetComponent<Rigidbody2D>().velocity = new Vector3(direction.x, direction.y, 0).normalized * bulletspeed;
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            selfRb.velocity = Vector3.zero;
-            //GPdamage = 0f;
-            Gslam = true;
-        }
         
+        if (Input.GetKeyUp(KeyCode.A) && Mathf.Abs(Rb.velocity.x) < buttonMinspeed) Rb.velocity = new Vector3(0,Rb.velocity.y,0);
+        if (Input.GetKeyUp(KeyCode.D) && Mathf.Abs(Rb.velocity.x) < buttonMinspeed) Rb.velocity = new Vector3(0, Rb.velocity.y, 0);
+        if (Input.GetKeyUp(KeyCode.S) && Mathf.Abs(Rb.velocity.y) < buttonMinspeed) Rb.velocity = new Vector3(Rb.velocity.x, 0, 0);
+        if (Input.GetKeyUp(KeyCode.W) && Mathf.Abs(Rb.velocity.y) < buttonMinspeed) Rb.velocity = new Vector3(Rb.velocity.x, 0, 0);
+
+        if (Input.GetKey(KeyCode.Space)) { 
+            if(Mathf.Abs(Rb.velocity.x) < buttonMinspeed) Rb.velocity = new Vector3(0, Rb.velocity.y, 0);
+            if(Mathf.Abs(Rb.velocity.y) < buttonMinspeed) Rb.velocity = new Vector3(Rb.velocity.x, 0, 0);
+            Rb.velocity = new Vector3(Rb.velocity.x/2*Time.deltaTime, Rb.velocity.y/2* Time.deltaTime, 0); }
+
+        Debug.Log(Rb.velocity);
     }
 
     private void FixedUpdate()
-    { 
-
-        if (Input.GetKey(KeyCode.A) && selfRb.velocity.x > -4.9f) selfRb.AddForce(Vector3.left * 20, ForceMode2D.Force);
-        if (Input.GetKey(KeyCode.D) && selfRb.velocity.x < 4.9f) selfRb.AddForce(Vector3.right * 20, ForceMode2D.Force);
-        if (Gslam)
-        {
-            RaycastHit2D ray2ground = Physics2D.Raycast(selfRb.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-            if (ray2ground.collider != null && ray2ground.distance <= 0.5f)
-            {
-                selfRb.velocity = Vector3.zero;
-                Gslam = false;
-            }
-            else {
-                selfRb.AddForce(Vector3.down * 50, ForceMode2D.Force);
-            }
-        }
+    {
+        if (Input.GetKey(KeyCode.A) && Rb.velocity.x > -buttonMaxspeed) Rb.AddForce(Vector3.left * 20, ForceMode2D.Force);
+        if (Input.GetKey(KeyCode.D) && Rb.velocity.x < buttonMaxspeed) Rb.AddForce(Vector3.right * 20, ForceMode2D.Force);
+        if (Input.GetKey(KeyCode.S) && Rb.velocity.y > -buttonMaxspeed) Rb.AddForce(Vector3.down * 20, ForceMode2D.Force);
+        if (Input.GetKey(KeyCode.W) && Rb.velocity.y < buttonMaxspeed) Rb.AddForce(Vector3.up * 20, ForceMode2D.Force);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -76,9 +75,8 @@ public class player : MonoBehaviour
             enemy e = other.gameObject.GetComponent<enemy>();
             if (e != null)
             {
-                e.hit(Mathf.Floor((Mathf.Abs(selfRb.velocity.x)/20f + Mathf.Abs(selfRb.velocity.y)/20f)*10f)/10f);
-                Debug.Log("충돌 velocity "+selfRb.velocity);
-                if (Gslam) {Gslam = false;}
+                e.hit(Mathf.Floor((Mathf.Abs(Rb.velocity.x)/20f + Mathf.Abs(Rb.velocity.y)/20f)*10f)/10f);
+                Debug.Log("충돌 velocity "+Rb.velocity);
             }
         }
     }
